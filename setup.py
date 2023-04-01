71b47c63dba3fa0c95b4cdad71f0277ce5146f56
bavo.denys <bavodenys@gmail.com>
2023-04-01 15:37:59 +0200

    Fixing setup file

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='Bade_python_example_project',
+    name='Python_example_project',
     version='0.1.0',
     description='Sample package for Python-Guide.org',
     long_description=readme,
     author='Bavo Denys',
-    author_email='bavo.denys@tremec.com',
-    url='https://bitbucket.org/controls-tremec/sandboxbade',
+    author_email='',
+    url='https://github.com/bavodenys/python_project_example',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
